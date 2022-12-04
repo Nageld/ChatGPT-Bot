8df60442e802e30ef441fd5927873675d2be5ae9
Nageld <Nageld@bxscience.edu>
2022-12-03 23:53:31 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -6,13 +6,15 @@ const command = {
         .setDescription('Prompt for the bot')
         .addStringOption(option => option.setName('input').setDescription('The prompt')),
     async execute(interaction, api) {
-        interaction.reply("Thinking about it")
-        const value = interaction.options.getString('input');        
+        const value = interaction.options.getString('input');     
+        interaction.reply(value)
         const response = await api.sendMessage(
             value
           )
-        return interaction.editReply("Prompt: " +value+ "\n"+response);
+        let output = response.length + value.length > 1980 ? 
+        response.substring(0, 2000-value.length-20): 
+        response;
+        return interaction.editReply("Prompt: " +value+ "\n"+output);
     },
-
 };
 export default command
