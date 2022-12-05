e653460aaebfd70952504506479174338afcfb09
JohnCorby <williamforgames6@gmail.com>
2022-12-04 23:08:10 -0800

    me stupid

--- src/apis.ts
+++ src/apis.ts
@@ -2,7 +2,7 @@ import { ChatGPTAPI } from "chatgpt";
 import { OpenAIApi, Configuration } from "openai";
 import { loadConfig } from "./utils.js";
 
-const config = await loadConfig();
+const config = loadConfig();
 
 export const openai = new OpenAIApi(new Configuration({ apiKey: config.openai }));
 export const chatgpt = new ChatGPTAPI({ headless: true });
