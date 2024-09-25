91ef9a53d344c85c74996db08744d375e0b435c5
James Weng <wengj@umich.edu>
2024-09-24 20:58:18 -0700

    Remove button again

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Client, Events, GatewayIntentBits, Interaction } from "discord.js";
 import { processQueueLoop } from "./commands/prompt.js";
-import { collectButtons, collectCommands, loadConfig } from "./utils.js";
+import { collectCommands, loadConfig } from "./utils.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const commands = await collectCommands();
 const commandMap = Object.fromEntries(commands.map((command) => [command.data.name, command]));
 
-const buttons = await collectButtons();
-const buttonMap = Object.fromEntries(buttons.map((button) => [button.id, button]));
+// const buttons = await collectButtons();
+// const buttonMap = Object.fromEntries(buttons.map((button) => [button.id, button]));
 
 const clientReady = async () => {
     console.log("Ready!");
@@ -23,9 +23,8 @@ const interactionCreateCallback = async (interaction: Interaction) => {
 
         await command.execute(interaction);
     } else if (interaction.isButton()) {
-        const button = buttonMap[interaction.customId];
-
-        await button.execute(interaction);
+        // const button = buttonMap[interaction.customId];
+        // await button.execute(interaction);
     }
 };
 client.on(Events.InteractionCreate, (interaction) => void interactionCreateCallback(interaction));
